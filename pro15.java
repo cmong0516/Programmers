c73898ea154bc66c662bf9ebfce4d8b90fa92f5b
cmong0516 <cmong0516@gmail.com>
2022-03-12 07:04:29 +0900

    2022.3.12

---
+++ pro15.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        HashSet<Integer> set = new HashSet<>();
+        for (int i : nums) {
+            set.add(i);
+        }
+        if (nums.length / 2 >= set.size())
+            answer = set.size();
+        else
+            answer = nums.length / 2;
+        return answer;
+    }
+}
