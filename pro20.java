2e52243ea29b7e3b351847c29e72c03c38ed76ae
cmong0516 <cmong0516@gmail.com>
2022-05-01 21:43:41 +0900

    2022.5.1

---
+++ pro20.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+
+class solution {
+    public static void main(String[] args) {
+        int[] numbers = { 2, 1, 3, 4, 1 };
+        solution(numbers);
+    }
+
+    public static int[] solution(int[] numbers) {
+        int[] answer = {};
+        HashSet<Integer> list = new HashSet<>();
+        for (int i = 0; i < numbers.length; i++) {
+            // System.out.println("numbers[i] ====== " + numbers[i]);
+            for (int j = i + 1; j < numbers.length; j++) {
+                // System.out.println("numbers[j] = " + numbers[j]);
+                list.add(numbers[i] + numbers[j]);
+            }
+        }
+        answer = list.stream().sorted().mapToInt(Integer::intValue).toArray();
+
+        // System.out.println("list = " + list);
+
+        return answer;
+    }
+}
