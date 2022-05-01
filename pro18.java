7ef8e4af3f83d7524c4fd7dccd3b35fc04b5be25
cmong0516 <cmong0516@gmail.com>
2022-05-01 20:53:17 +0900

    2022.5.1

---
+++ pro18.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+class solution {
+    public static void main(String[] args) {
+        solution(125);
+    }
+
+    public static int solution(int n) {
+        int answer = 0;
+        int a = 3;
+        ArrayList<Integer> list = new ArrayList<>();
+        while (n != 0) {
+            list.add(n % 3);
+            n /= 3;
+        }
+
+        Collections.reverse(list);
+
+        System.out.println("list.get(0) = " + list.get(0));
+
+        for (int i = 1; i < list.size(); i++) {
+            System.out.println("list.get(i) = " + list.get(i));
+            answer = answer + (a * list.get(i));
+            a *= 3;
+        }
+        answer += list.get(0);
+
+        System.out.println("answer = " + answer);
+        return answer;
+    }
+}
