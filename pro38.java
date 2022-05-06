99f9cb2f44cb93269bf927fc07792efafb9fe357
cmong0516 <cmong0516@gmail.com>
2022-05-07 07:05:21 +0900

    2022.5.7

---
+++ pro38.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class solution {
+    public static void main(String[] args) {
+        String s = "-1234";
+        solution1(s);
+    }
+
+    public static int solution1(String s) {
+        int answer = 0;
+
+        answer = Integer.parseInt(s);
+
+        return answer;
+    }
+}
