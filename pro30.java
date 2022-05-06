722fd13e1f248efcc3e5aa6f7923d096d4b5fbeb
cmong0516 <cmong0516@gmail.com>
2022-05-07 03:09:41 +0900

    2022.5.7

---
+++ pro30.java
@@ -0,0 +1,28 @@
+class solution {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        solution1(a, b);
+    }
+
+    public static long solution1(int a, int b) {
+        long answer = 0;
+
+        int temp = 0;
+
+        if (b < a) {
+            temp = b;
+            b = a;
+            a = temp;
+        }
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        for (int i = a; i <= b; i++) {
+            answer += i;
+        }
+        return answer;
+    }
+
+}
