87b717d1ae92b60cbb16b7e72809ac2cac2816cb
cmong0516 <cmong0516@gmail.com>
2022-05-06 11:35:22 +0900

    2022.5.6

---
+++ pro28.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+class solution {
+    public static void main(String[] args) {
+        int[] arr = { 1, 1, 3, 3, 0, 1, 1 };
+        solution1(arr);
+    }
+
+    public static int[] solution1(int[] arr) {
+
+        int temp = -1;
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != temp) {
+                list.add(arr[i]);
+                temp = arr[i];
+            }
+        }
+        int[] answer = new int[list.size()];
+
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+
+        return answer;
+    }
+
+}
