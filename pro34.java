c3cbfe83fcf849139f40470839f67a51dc30c5dd
cmong0516 <cmong0516@gmail.com>
2022-05-07 05:15:15 +0900

    2022.5.7

---
+++ pro34.java
@@ -0,0 +1,27 @@
+class solution {
+    public static void main(String[] args) {
+        String s = "a234";
+        System.out.println(solution1(s));
+    }
+
+    public static boolean solution1(String s) {
+        boolean answer = true;
+
+        char[] list = s.toCharArray();
+        char a = '0';
+        char b = '9';
+        // System.out.println((int)a);
+        // System.out.println((int)b);
+
+        for (char c : list) {
+            if ((int) c > b) {
+                answer = false;
+            }
+        }
+        if (s.length() != 4 && s.length() != 6) {
+            answer = false;
+        }
+        return answer;
+    }
+
+}
