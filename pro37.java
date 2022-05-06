adf74f8daa0d8d741ea12a70736aa7beb8c29789
cmong0516 <cmong0516@gmail.com>
2022-05-07 06:58:43 +0900

    2022.5.7

---
+++ pro37.java
@@ -0,0 +1,25 @@
+class solution {
+    public static void main(String[] args) {
+        int n = 3;
+        solution1(n);
+    }
+
+    public static String solution1(int n) {
+        String answer = "";
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                sb.append("수");
+            }
+            if (i % 2 == 1) {
+                sb.append("박");
+            }
+        }
+        // System.out.println("sb = " + sb);
+        answer = String.valueOf(sb);
+
+        return answer;
+    }
+}
