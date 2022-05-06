de0ac4281da6a434ab5501bf51382a8c300ca03f
cmong0516 <cmong0516@gmail.com>
2022-05-07 05:02:31 +0900

    2022.5.7

---
+++ pro33.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+class solution {
+    public static void main(String[] args) {
+        String s = "Zbcdefg";
+        solution1(s);
+    }
+
+    public static String solution1(String s) {
+        String answer = "";
+
+        char[] split = s.toCharArray();
+        int[] intS = new int[split.length];
+        for (int i = 0; i < s.length(); i++) {
+            intS[i] = split[i];
+        }
+
+        Arrays.sort(intS);
+
+        for (int i = 0; i < s.length() / 2; i++) {
+            int temp = intS[i];
+            intS[i] = intS[intS.length - i - 1];
+            intS[intS.length - i - 1] = temp;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            sb.append((char) intS[i]);
+        }
+        // System.out.println("sb = " + sb);
+
+        answer = String.valueOf(sb);
+
+        return answer;
+    }
+
+}
