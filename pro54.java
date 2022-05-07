17b347ea9936c6cfa76e51b538adb4c4773be705
cmong0516 <cmong0516@gmail.com>
2022-05-08 06:29:30 +0900

    2022.5.8

---
+++ pro54.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class solution {
+    public static void main(String[] args) {
+        int x = 2;
+        int n = 5;
+        solution1(x, n);
+    }
+
+    public static long[] solution1(int x, int n) {
+        long[] answer = new long[n];
+
+        for (int i = 0; i < n; i++) {
+            answer[i] = (long) x * (i + 1);
+        }
+
+        return answer;
+    }
+}
