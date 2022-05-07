19c241b0ea09ec4e9e29c982a0520e28a428c868
cmong0516 <cmong0516@gmail.com>
2022-05-08 06:21:16 +0900

    2022.5.8

---
+++ pro52.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class solution {
+    public static void main(String[] args) {
+        String phone_number = "01033334444";
+        solution1(phone_number);
+    }
+
+    public static String solution1(String phone_number) {
+        String answer = "";
+
+        String front = phone_number.substring(0, phone_number.length() - 4).replaceAll(".", "*");
+
+        answer = front + phone_number.substring(phone_number.length() - 4);
+
+        return answer;
+    }
+}
