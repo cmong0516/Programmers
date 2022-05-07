8722a4da5d5d3c928aa2e7e12f2ab82ba9fe2574
cmong0516 <cmong0516@gmail.com>
2022-05-08 06:06:58 +0900

    2022.5.8

---
+++ pro51.java
@@ -0,0 +1,25 @@
+class solution {
+    public static void main(String[] args) {
+        int x = 11;
+        solution1(x);
+    }
+
+    public static boolean solution1(int x) {
+        boolean answer = true;
+        int total = 0;
+        int y = x;
+        while (x > 0) {
+            total += x % 10;
+            x /= 10;
+        }
+
+        System.out.println("total = " + total);
+        if (y % total != 0) {
+            answer = false;
+        }
+
+        return answer;
+    }
+}
+
+// 에이 ㅋ
