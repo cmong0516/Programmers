02b14aab7e209e68a44458cea8aa757ac2418c03
cmong0516 <cmong0516@gmail.com>
2022-05-08 05:36:07 +0900

    2022.5.8

---
+++ pro48.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class solution {
+    public static void main(String[] args) {
+        int n = 3;
+        int m = 12;
+        solution1(n, m);
+    }
+
+    public static int[] solution1(int n, int m) {
+        int[] answer = new int[2];
+
+        int max = Math.max(n, m);
+        int min = Math.min(n, m);
+
+        while (min != 0) {
+            int temp = max % min;
+            max = min;
+            min = temp;
+        }
+
+        answer[0] = max;
+
+        answer[1] = max * (n / max) * (m / max);
+
+        System.out.println("answer = " + Arrays.toString(answer));
+
+        return answer;
+    }
+}
