1be3f71b9c38195fbf67623799799d277811c535
cmong0516 <cmong0516@gmail.com>
2022-05-07 23:30:43 +0900

    2022.5.7

---
+++ pro40.java
@@ -0,0 +1,18 @@
+class solution {
+    public static void main(String[] args) {
+        int n = 12;
+        solution1(n);
+    }
+
+    public static int solution1(int n) {
+        int answer = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                answer += i;
+            }
+        }
+
+        return answer;
+    }
+}
