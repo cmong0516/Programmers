66a8e0881c54c386faa34839ee0b573abc6274f4
cmong0516 <cmong0516@gmail.com>
2022-05-08 02:51:33 +0900

    2022.5.8

---
+++ pro45.java
@@ -0,0 +1,18 @@
+class solution {
+    public static void main(String[] args) {
+        long n = 121;
+        solution1(n);
+    }
+
+    public static long solution1(long n) {
+        long answer = 0;
+
+        if (Math.sqrt(n) % 1 == 0) {
+            answer = (long) (Math.sqrt(n) + 1) * (long) (Math.sqrt(n) + 1);
+        } else {
+            answer = -1;
+        }
+
+        return answer;
+    }
+}
