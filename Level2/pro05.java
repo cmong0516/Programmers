eb0dd315422b4c88b36f17290fd7c3768eedb089
cmong0516 <cmong0516@gmail.com>
2022-05-12 23:04:29 +0900

    2022.5.12

---
+++ Level2/pro05.java
@@ -0,0 +1,13 @@
+import java.math.BigInteger;
+
+class Solution {
+    public long solution(int w, int h) {
+        long answer = 1;
+
+        int gcd = BigInteger.valueOf(w).gcd(BigInteger.valueOf(h)).intValue();
+        answer = ((long) w * (long) h) - (((long) w / gcd) + ((long) h / gcd) - 1) * gcd;
+        // System.out.println("answer = " + answer);
+
+        return answer;
+    }
+}
